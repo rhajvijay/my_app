99518f126a3afd533592673f1d4f08228993a92b
rhajvijay <rhajvijay@gmail.com>
2024-12-26 22:23:58 +0530

    initial app

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple[200],
+        body: Center(
+         child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(20)
+            ),
+            padding: EdgeInsets.all(25),
+            child: Text("Ansen Magizhan"),
+
+          ),
+        ),
+      ),
+    );
+  }
+}
